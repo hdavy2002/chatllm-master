6fc16964081d7ef2fff08cbcdf751013b66cd165
ChatLTT <hdavy2002@gmail.com>
2024-07-18 20:08:26 +0530

    Update tailwind.config.js

--- embed/tailwind.config.js
+++ embed/tailwind.config.js
@@ -31,7 +31,7 @@ export default {
         "historical-msg-system": "rgba(255, 255, 255, 0.05);",
         "historical-msg-user": "#2C2F35",
         outline: "#4E5153",
-        "primary-button": "#ff5757",
+        "primary-button": "#46C8FF",
         secondary: "#2C2F36",
         "dark-input": "#18181B",
         "mobile-onboarding": "#2C2F35",
