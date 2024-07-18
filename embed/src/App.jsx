bdead48c7e7386c19a17552cee40e1dbd5883560
ChatLTT <hdavy2002@gmail.com>
2024-07-18 19:41:50 +0530

    Update App.jsx

--- embed/src/App.jsx
+++ embed/src/App.jsx
@@ -6,7 +6,7 @@ import OpenButton from "@/components/OpenButton";
 import ChatWindow from "./components/ChatWindow";
 import { useEffect } from "react";
 
-export default function App() {
+export default function App({ containerId }) {
   const { isChatOpen, toggleOpenChat } = useOpenChat();
   const embedSettings = useGetScriptAttributes();
   const sessionId = useSessionId();
@@ -19,14 +19,6 @@ export default function App() {
 
   if (!embedSettings.loaded) return null;
 
-  const positionClasses = {
-    "bottom-left": "allm-bottom-0 allm-left-0 allm-ml-4",
-    "bottom-right": "allm-bottom-0 allm-right-0 allm-mr-4",
-    "top-left": "allm-top-0 allm-left-0 allm-ml-4 allm-mt-4",
-    "top-right": "allm-top-0 allm-right-0 allm-mr-4 allm-mt-4",
-  };
-
-  const position = embedSettings.position || "bottom-right";
   const windowWidth = embedSettings.windowWidth ?? "400px";
   const windowHeight = embedSettings.windowHeight ?? "700px";
 
@@ -34,30 +26,22 @@ export default function App() {
     <>
       <Head />
       <div
-        id="anything-llm-embed-chat-container"
-        className={`allm-fixed allm-inset-0 allm-z-50 ${isChatOpen ? "allm-block" : "allm-hidden"}`}
+        id={containerId}
+        className={`${isChatOpen ? "allm-block" : "allm-hidden"}`}
+        style={{ width: windowWidth, height: windowHeight }}
       >
-        <div
-          style={{
-            maxWidth: windowWidth,
-            maxHeight: windowHeight,
-          }}
-          className={`allm-h-full allm-w-full allm-bg-white allm-fixed allm-bottom-0 allm-right-0 allm-mb-4 allm-md:mr-4 allm-rounded-2xl allm-border allm-border-gray-300 allm-shadow-[0_4px_14px_rgba(0,0,0,0.25)] ${positionClasses[position]}`}
-          id="anything-llm-chat"
-        >
-          {isChatOpen && (
-            <ChatWindow
-              closeChat={() => toggleOpenChat(false)}
-              settings={embedSettings}
-              sessionId={sessionId}
-            />
-          )}
-        </div>
+        {isChatOpen && (
+          <ChatWindow
+            closeChat={() => toggleOpenChat(false)}
+            settings={embedSettings}
+            sessionId={sessionId}
+          />
+        )}
       </div>
       {!isChatOpen && (
         <div
           id="anything-llm-embed-chat-button-container"
-          className={`allm-fixed allm-bottom-0 ${positionClasses[position]} allm-mb-4 allm-z-50`}
+          className="allm-fixed allm-bottom-0 allm-right-0 allm-mb-4 allm-z-50"
         >
           <OpenButton
             settings={embedSettings}
