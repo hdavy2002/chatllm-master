1a2dfd30500d47fa92f3887e65f5a464154c480b
ChatLTT <hdavy2002@gmail.com>
2024-07-26 19:07:26 +0530

    query

--- frontend/src/pages/WorkspaceSettings/ChatSettings/ChatModeSelection/index.jsx
+++ frontend/src/pages/WorkspaceSettings/ChatSettings/ChatModeSelection/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+
 export default function ChatModeSelection({ workspace, setHasChanges }) {
-  const [chatMode, setChatMode] = useState(workspace?.chatMode || "chat");
+  const [chatMode] = useState("query");
   const { t } = useTranslation();
+
   return (
     <div>
       <div className="flex flex-col">
@@ -16,43 +18,17 @@ export default function ChatModeSelection({ workspace, setHasChanges }) {
           <input type="hidden" name="chatMode" value={chatMode} />
           <button
             type="button"
-            disabled={chatMode === "chat"}
-            onClick={() => {
-              setChatMode("chat");
-              setHasChanges(true);
-            }}
-            className="transition-bg duration-200 px-6 py-1 text-md text-white/60 disabled:text-white bg-transparent disabled:bg-[#687280] rounded-md"
-          >
-            {t("chat.mode.chat.title")}
-          </button>
-          <button
-            type="button"
-            disabled={chatMode === "query"}
-            onClick={() => {
-              setChatMode("query");
-              setHasChanges(true);
-            }}
-            className="transition-bg duration-200 px-6 py-1 text-md text-white/60 disabled:text-white bg-transparent disabled:bg-[#687280] rounded-md"
+            disabled={true}
+            className="transition-bg duration-200 px-6 py-1 text-md text-white disabled:bg-[#687280] rounded-md"
           >
             {t("chat.mode.query.title")}
           </button>
         </div>
         <p className="text-sm text-white/60">
-          {chatMode === "chat" ? (
-            <>
-              <b>{t("chat.mode.chat.title")}</b>{" "}
-              {t("chat.mode.chat.desc-start")}{" "}
-              <i className="font-semibold">{t("chat.mode.chat.and")}</i>{" "}
-              {t("chat.mode.chat.desc-end")}
-            </>
-          ) : (
-            <>
-              <b>{t("chat.mode.query.title")}</b>{" "}
-              {t("chat.mode.query.desc-start")}{" "}
-              <i className="font-semibold">{t("chat.mode.query.only")}</i>{" "}
-              {t("chat.mode.query.desc-end")}
-            </>
-          )}
+          <b>{t("chat.mode.query.title")}</b>{" "}
+          {t("chat.mode.query.desc-start")}{" "}
+          <i className="font-semibold">{t("chat.mode.query.only")}</i>{" "}
+          {t("chat.mode.query.desc-end")}
         </p>
       </div>
     </div>
